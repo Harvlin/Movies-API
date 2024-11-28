a6d3a5b4f291ad7a9c484197451c74e9c82fbcaf
Harvlin Maximillian <harvlinmaximillian@gmail.com>
2024-11-28 10:54:27 +0700

    Update TestDataUtil.java

--- src/test/java/com/project/Movie/Collections/TestDataUtil.java
+++ src/test/java/com/project/Movie/Collections/TestDataUtil.java
@@ -7,26 +7,5 @@ import com.project.Movie.Collections.domain.dto.MoviesDto;
 public class TestDataUtil {
     private TestDataUtil() {}
 
-    public static GenreDto createGenreDto() {
-        return GenreDto.builder()
-                .names("Sci-fi")
-                .movies(null)
-                .build();
-    }
-
-    public static DirectorDto createDirectorDto() {
-        return DirectorDto.builder()
-                .name("Christopher Nolan")
-                .age(50)
-                .moviesTitles(null)
-                .build();
-    }
-    public static MoviesDto createTestMovieDto(final GenreDto genreDto, final DirectorDto directorDto) {
-        return MoviesDto.builder()
-                .title("Inception")
-                .description("This is a sci-fi movie")
-                .genre(genreDto)
-                .director(directorDto)
-                .build();
-    }
+    
 }
